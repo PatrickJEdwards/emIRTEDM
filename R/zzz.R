22e88e49044791459e8f6a00eaf62e21fe4318f9
Patrick Edwards <99101545+PatrickJEdwards@users.noreply.github.com>
2025-11-11 14:19:17 -0600

    Update emIRT package name so that it doesn't overwrite my existing installation of the emIRT package.

--- R/zzz.R
+++ R/zzz.R
@@ -4,11 +4,11 @@
    x <- regexpr("[0-9]{4}", mydate)
    this.year <- substr(mydate, x[1], x[1] + attr(x, "match.length") - 1)
 
-  packageStartupMessage("\n## emIRT: Fast Estimation of Ideal Points with Massive Data")
+  packageStartupMessage("\n## emIRT_EDM: Fast Estimation of Ideal Points with Massive Data")
    packageStartupMessage("## 2014 - ", this.year)
    packageStartupMessage("## Kosuke Imai, James Lo, and Jonathan Olmsted\n\n")
 }
 
 .onUnload <- function(libpath) {
-    library.dynam.unload("emIRT", libpath)
+    library.dynam.unload("emIRT_EDM", libpath)
 }
